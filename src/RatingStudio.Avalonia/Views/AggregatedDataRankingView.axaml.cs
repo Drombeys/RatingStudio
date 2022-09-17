61006d5603dd698284a5f424b4bf115f649391fb
Drombeys <dima.zolotov02@gmail.com>
2022-09-17 22:29:13 +0300

    Add `AggregatedDataRankingView` page

---
+++ src/RatingStudio.Avalonia/Views/AggregatedDataRankingView.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RatingStudio.Avalonia.Views;
+
+public partial class AggregatedDataRankingView : UserControl
+{
+    public AggregatedDataRankingView() => InitializeComponent();
+
+    private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+}
