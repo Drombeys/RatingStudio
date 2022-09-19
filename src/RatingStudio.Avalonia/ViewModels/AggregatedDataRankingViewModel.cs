308cf7306a4b7057d86a5de1b3c1cb87ac820b79
Drombeys <dima.zolotov02@gmail.com>
2022-09-19 22:24:39 +0300

    Add university rating output

--- src/RatingStudio.Avalonia/ViewModels/AggregatedDataRankingViewModel.cs
+++ src/RatingStudio.Avalonia/ViewModels/AggregatedDataRankingViewModel.cs
@@ -1,10 +1,19 @@
-﻿namespace RatingStudio.Avalonia.ViewModels;
+﻿using System.Collections.ObjectModel;
+using RatingStudio.Domain.Dto;
+
+namespace RatingStudio.Avalonia.ViewModels;
 
 /// <summary>
 /// Aggregated data ranking view model
 /// </summary>
 public class AggregatedDataRankingViewModel : ViewModelBase
 {
+    #region Public Properties
+
+    public ObservableCollection<UniversityRatingDto> UniversityRating { get; private set; } = new();
+
+    #endregion
+    
     #region Constructor
 
     /// <summary>
---
+++ src/RatingStudio.Domain/Dto/UniversityRatingDto.cs
@@ -0,0 +1,30 @@
+﻿namespace RatingStudio.Domain.Dto;
+
+/// <summary>
+/// Представление рейтинга университетов
+/// </summary>
+/// <param name="Name">Название учебного заведения</param>
+/// <param name="ARWU">Рейтинг "ARWU"</param>
+/// <param name="QS">Рейтинг "QS"</param>
+/// <param name="THE">Рейтинг "THE"</param>
+/// <param name="ScimagoIR">Рейтинг "ScimagoIR"</param>
+/// <param name="NTU">Рейтинг "NTU"</param>
+/// <param name="URAP">Рейтинг "URAP"</param>
+/// <param name="CWTS">Рейтинг "CWTS"</param>
+/// <param name="USNews">Рейтинг "USNews"</param>
+/// <param name="MosIUR">Рейтинг "MosIUR"</param>
+/// <param name="Rank">Ранг учебного заведения</param>
+public record UniversityRatingDto
+(
+    string? Name,
+    int ARWU, 
+    int QS, 
+    int THE, 
+    int ScimagoIR, 
+    int NTU, 
+    int URAP, 
+    int CWTS, 
+    int USNews, 
+    int MosIUR, 
+    double Rank
+);
